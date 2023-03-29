254344d0b57f40b14e00ff633fc782a557012f7b
Tanyapavliuk <122897433+Tanyapavliuk@users.noreply.github.com>
2023-03-29 11:24:36 +0300

    correct

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,14 +1,14 @@
 
  new Swiper ('.swiper', {
-  spaceBetween: 0,
+  spaceBetween: 18,
   grabCursor: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: '.pagination-products',
     clickable: true,
    },
    zoom: {
      maxRatio: 1.5,
-     minRatio: 1,
+     minRatio: 1
   },
   breakpoints :{
     375 :{
@@ -19,6 +19,26 @@
     },
     1200 :{
       slidesPerView: 4,
+    },
+  }
+});
+
+new Swiper ('.swiper-buy', {
+  spaceBetween: 16,
+  grabCursor: true,
+  pagination: {
+    el: '.pagination-buy',
+    clickable: true,
+   },
+  breakpoints :{
+    375 :{
+      slidesPerView: 1,
+    },
+    758 :{
+      slidesPerView: 1,
+    },
+    1200 :{
+      slidesPerView: 3,
     },
   }
 });
