4a2352543a554571ad0f051da853d0d0d9969bd2
Tanyapavliuk <122897433+Tanyapavliuk@users.noreply.github.com>
2023-03-30 12:23:13 +0300

    Revert "Merge branch 'main' into error-validator"
    This reverts commit 1efcf3f8b992ff6734c20f84ae011bc1175e63fa, reversing
    changes made to eee11f8b6590db3e333bf66678a4f1e91052655c.

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,14 +1,10 @@
 
  new Swiper ('.swiper', {
-  spaceBetween: 18,
+  spaceBetween: 0,
   grabCursor: true,
   pagination: {
-    el: '.pagination-products',
+    el: '.swiper-pagination',
     clickable: true,
-   },
-   zoom: {
-     maxRatio: 1.5,
-     minRatio: 1
   },
   breakpoints :{
     375 :{
@@ -19,26 +15,6 @@
     },
     1200 :{
       slidesPerView: 4,
-    },
-  }
-});
-
-new Swiper ('.swiper-buy', {
-  spaceBetween: 16,
-  grabCursor: true,
-  pagination: {
-    el: '.pagination-buy',
-    clickable: true,
-   },
-  breakpoints :{
-    375 :{
-      slidesPerView: 1,
-    },
-    758 :{
-      slidesPerView: 1,
-    },
-    1200 :{
-      slidesPerView: 3,
     },
   }
 });
